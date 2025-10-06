7c6601e783d8841d32302e939349df4d5d88836d
Fabrizio Meinero <fmei@ciklum.com>
2025-10-06 10:33:00 +0200

    feat: load oh-my-posh light and dark theme

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,8 +6,15 @@ $ps_profile_home = "$Env:USERPROFILE\Documents\PowerShell"
 # Oh My Posh profile
 # oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/spaceship.omp.json" | Invoke-Expression
 # oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/agnoster-minimal.omp.json" | Invoke-Expression
-# oh-my-posh init pwsh --config "$ps_profile_home/oh-my-posh/themes/custom-agnoster-minimal.json" | Invoke-Expression
-oh-my-posh init pwsh --config "$ps_profile_home/oh-my-posh/themes/custom-spaceship.json" | Invoke-Expression
+
+# Detect Windows theme (dark/light) and set Oh My Posh theme accordingly
+$theme = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme"
+if ($theme -eq 0) {
+    oh-my-posh init pwsh --config "$ps_profile_home/oh-my-posh/themes/custom-spaceship-dark.json" | Invoke-Expression
+} else {
+    oh-my-posh init pwsh --config "$ps_profile_home/oh-my-posh/themes/custom-spaceship-light.json" | Invoke-Expression
+}
+
 
 # Icons
 Import-Module -Name Terminal-Icons
