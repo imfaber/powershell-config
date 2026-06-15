bd6e7e226cd1f8a8a2f3d52dad92dff0aced74dd
Fabrizio Meinero <fmei@ciklum.com>
2026-06-15 15:14:35 +0200

    Add Yazi support

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -21,3 +21,16 @@ Import-Module -Name Terminal-Icons
 
 # Aliases
 . "$ps_profile_home\Aliases.ps1"
+
+
+# Yazi
+# provides the ability to change the current working directory when exiting Yazi.
+function y {
+	$tmp = (New-TemporaryFile).FullName
+	yazi.exe @args --cwd-file="$tmp"
+	$cwd = Get-Content -Path $tmp -Encoding UTF8
+	if ($cwd -and $cwd -ne $PWD.Path -and (Test-Path -LiteralPath $cwd -PathType Container)) {
+		Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+	}
+	Remove-Item -Path $tmp
+}
